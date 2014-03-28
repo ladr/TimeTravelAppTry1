325e3f042e053bcac71d661eec5e12f0a44e4273
ladr <vladl@list.ru>
2014-03-28 23:04:46 +0200

    Initial commit

--- apptry1/src/main/java/com/ladr/timetravel/apptry1/MainActivity.java
+++ apptry1/src/main/java/com/ladr/timetravel/apptry1/MainActivity.java
@@ -1,18 +1,95 @@
 package com.ladr.timetravel.apptry1;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 
 public class MainActivity extends ActionBarActivity {
+    private TextView tvDisplayTime;
+    private Button btnChangeTime;
+
+    private int hour;
+    private int minute;
+
+    static final int TIME_DIALOG_ID = 999;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setCurrentTimeOnView();
+        addListenerOnButton();
     }
+
+
+    // display current time
+    public void setCurrentTimeOnView() {
+
+        tvDisplayTime = (TextView) findViewById(R.id.tvTime);
+
+
+        final Calendar c = Calendar.getInstance();
+        hour = c.get(Calendar.HOUR_OF_DAY);
+        minute = c.get(Calendar.MINUTE);
+
+        // set current time into textview
+        tvDisplayTime.setText(
+                new StringBuilder().append(pad(hour))
+                        .append(pad(minute)).append(" AD"));
+    }
+
+    public void addListenerOnButton() {
+        btnChangeTime = (Button) findViewById(R.id.btnChangeTime);
+
+        btnChangeTime.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                showDialog(TIME_DIALOG_ID);
+
+            }
+
+        });
+
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case TIME_DIALOG_ID:
+                // set time picker as current time
+                return new TimePickerDialog(this,
+                        timePickerListener, hour, minute,true);
+
+        }
+        return null;
+    }
+
+    private TimePickerDialog.OnTimeSetListener timePickerListener =
+            new TimePickerDialog.OnTimeSetListener() {
+                public void onTimeSet(TimePicker view, int selectedHour,
+                                      int selectedMinute) {
+                    hour = selectedHour;
+                    minute = selectedMinute;
+
+                    // set current time into textview
+                    tvDisplayTime.setText(new StringBuilder().append(pad(hour))
+                            .append(pad(minute)).append(" AD"));
+                }
+            };
+
 
 
     @Override
@@ -33,6 +110,14 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    //Padding leading zero
+    private static String pad(int c) {
+        if (c >= 10)
+            return String.valueOf(c);
+        else
+            return "0" + String.valueOf(c);
     }
 
 }
